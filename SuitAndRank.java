43108e7cb31a9c34bfeff82d5677ad061ee6dd0e
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-17 23:17:08 +0530

    Added: suit and rank class for providing player with name of suit and rank like ace of spades

---
+++ SuitAndRank.java
@@ -0,0 +1,14 @@
+
+package com.bridgelabz.objectorientedprograms;
+
+/**
+ * 	Uc6 : array of rank suit values for providing player with index number.
+ *  This class is used for assigning suits and ranks value by using array.  
+ */
+ 
+class SuitAndRank {
+    
+    String suit[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    String Rank[] = { "2","3","4","5","6","7","8","9","10", "Jack","Queen","King","Ace"};
+    
+}
